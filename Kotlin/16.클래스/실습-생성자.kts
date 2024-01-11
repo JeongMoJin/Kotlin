7b347a647c4f32b6520c2a54c3856bdcf334b103
JeongMoJin <wlswjdah123@naver.com>
2024-01-11 18:26:12 +0900

    2024-01-11

--- Kotlin/16.클래스/실습-생성자.kts
+++ Kotlin/16.클래스/실습-생성자.kts
@@ -25,10 +25,89 @@ class User1 constructor(name:String){ // 클래스 네이밍은 대문자로 시
 // 객체 -> Object, Instance
 val user = User1("홍길동")
 
+// 주생성자 -> init을 생략하는 방법
+class User2 constructor(name : String) {
+    val userName : String = name
+}
+
+// 주생성자 -> constructor를 생략하는 방법
+class User3 constructor(name : String) {
+    val userName : String = name
+}
+// 주생성자 -> 생략할 수 있는 모든 걸 생략하는 방법
+class User3(val name: String) {
+
+}
+// 주생성자 -> 기본값
+class User4(name: String = "김아무개") {
+    val userName: String = name
+}
+val user = User4()
+
+// 생성자에서 받는 속성이 복수개인 경우
+class User5 constructor(age: Int, name:String) {
+    val age : Int // 클래스 속성
+    val name : String
+
+    init {
+        this.age = age // this는 클래스 자체를 의미한다 (User5) -> User5의 age
+        this.name = name
+    }
+}
+// .의 의미
+// -> ~의
+val user5 = User5(20, "홍길동")
+println(user5.age)
+// - user5.age -> user5의 age
+// - user5.name -> user5의 name
+// - user5.기능 -> user5의 기능
+// -> .은 객체의 속성이나 기능을 사용할 때 사용한다
+
+// 부생성자 (Secondary Constructor)
+// - constructor 키워드를 생략할 수 없다
+// - 주생성자에는 객체를 만들기 위한 필수 조건이 있다면, 부생정자에는 객체를 만들기 위한 옵션 조건이 있다
+// - 부생성자에는 주생성자에서 필요한 조건을 포함하고 있어야 한다 (파라미터를 포함하고 있어야한다)
+// - 부생성자는 주생성자에게 생성을 위임해야한다
+class User6 constructor(name : String) {
+    var age : Int = 0
+    var name : String
+
+    init {
+        println("init")
+        this.name = name
+    }
+    // 부생성자는 클래스명 우측에 올 수 없다 -> 클래스의 본문에 있어야 한다
+    constructor(name: String, age : Int) :this(name) {
+        println("second")
+        this.age = age
+    }
+}
+val user6 = User6(name = "가나다")
+println(user6.name)
+val user6_2 = User6("가나다", 50)
+println(user6_2.age)
+
+// 실행순서
+// 부생성자 호출 -> 부생성자 안에 있는 주생성자 호출 -> init 블록 호출 -> 부생성자 본문 실행
+// 클래스 속성에서 초기화를 시켜주지 않아도 되는 이유
+// - 초기화블록에서 초기화를 보장해주기 때문 -> 클래스가 생성될 때 초기화 블록은 무조건 실행된다
+// - 초기화블록에 없는 속성은 선언할 때 초기화 해줘야한다
 
 
+class User7 {
+    val age : Int
+    val name : String
 
+    constructor(age : Int, name : String) {
+        this.age = age
+        this.name = name
+    }
+}
 
-
-
+val user7 = User7(10, "가가가")
+println(User7.age)
+// 주생성자가 없는 것 처럼 보이고 부생성자만 있을 때 부생성자가 주생성자처럼 보인다
+// -> 부생성자가 맞다!!
+// -> 주생성자는 코틀린이 자동으로 만들어준다
+// -> 주생성자가 없기 때문에 this()생성자를 이용해 생성을 위임할 필요가 없다
 
